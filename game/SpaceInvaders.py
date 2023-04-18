40bb1145e9921ce40ba4dfb34ee0f0363ccee039
HillelPa <64850372+HillelPa@users.noreply.github.com>
2023-04-18 18:48:23 +0200

    Test d'une solution pour linux

--- game/SpaceInvaders.py
+++ game/SpaceInvaders.py
@@ -24,6 +24,8 @@ class SpaceInvaders():
     
     def __init__(self, target_score, no_invaders, display : bool = False, factor = 100):
 
+        os.environ["SDL_VIDEODRIVER"] = "dummy"
+        
         # player
         self.display = display
         
