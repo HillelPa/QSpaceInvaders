4ea6df02a825e8949535fe1c9a2ac7faf3c5624d
HillelPa <64850372+HillelPa@users.noreply.github.com>
2023-04-20 00:44:45 +0200

    QTABLES apres 3000 episodes et un gros Epsilon

---
+++ check_qtable.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+factor = 100
+
+qtable = np.load("controller/qtables/qtable"+str(factor)+".npy")
+
+for i in range(len(qtable)):
+            for j in range(len(qtable[i])):
+                for k in range(len(qtable[i][j])):
+                    for l in range(len(qtable[i][j][k])):
+                        
+                        for m in range(len(qtable[i][j][k][l])):
+                            for n in range(len(qtable[i][j][k][l][m])):
+                                print(f"pX : {i}, iX : {j}, iY : {k}, bX : {l}, bY : {m}, bS : {n}")
+                                for o in range(len(qtable[i][j][k][l][m][n])):
+                                    print("action : ", m, " = [",qtable[i][j][k][l][m][n][o], "]")
+
