7b819c5ddeb22cb11dc10fb3e1bb26cd192dba96
Partouche Hillel <hpartouche@tc405-112-16.insa-lyon.fr>
2023-04-23 02:42:41 +0200

    auto

--- run_and_commit.sh
+++ run_and_commit.sh
@@ -3,7 +3,7 @@ python3 run_game.py $1 $2 $3 0 --epsilon 0.4
 git add *
 git commit -m "auto"
 git push
-python3 run_game.py $1 $2 $3 0.1
+python3 run_game.py $1 $2 $3 0
 git add *
 git commit -m "auto"
 git push
