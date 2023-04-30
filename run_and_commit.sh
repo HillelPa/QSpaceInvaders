4e90c33cebc591c8c231ee757cb362eef392089b
HillelPa <64850372+HillelPa@users.noreply.github.com>
2023-04-30 13:52:54 +0200

    pour ssh

--- run_and_commit.sh
+++ run_and_commit.sh
@@ -1,9 +1,9 @@
 git pull
-python3 run_game.py $1 $2 $3 0 --epsilon 0.4
+python3 run_game.py $1 $2 $3 0 --epsilon 0.5
 git add *
 git commit -m "auto"
 git push
 python3 run_game.py $1 $2 $3 0
 git add *
-git commit -m "auto"
+git commit -m "auto 2"
 git push
