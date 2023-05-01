183f4922ffc1889c2cd5b6d9d5d7406d567d0ba0
HillelPa <64850372+HillelPa@users.noreply.github.com>
2023-05-01 18:29:05 +0200

    auto e = 0.2

--- run_and_commit.sh
+++ run_and_commit.sh
@@ -1,9 +1,9 @@
 git pull
-python3 run_game.py $1 $2 $3 0 
+python3 run_game.py $1 $2 $3 0 --epsilon 0.2
 git add *
-git commit -m "auto"
+git commit -m "auto e = 0.2"
 git push
-python3 run_game.py $1 $2 $3 0 --epsilon 0
+python3 run_game.py $1 $2 $3 0 --epsilon 0.4
 git add *
-git commit -m "auto avec epsilon = 0"
+git commit -m "auto avec epsilon = 0.4"
 git push
